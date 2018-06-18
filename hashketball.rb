15b62093f76cd1d982b3eebe243866f4669d9324
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 20:13:29 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ def game_hash
   { :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [
+      :players => {
         {
           "Alan Anderson" => {
             :number => "0",
