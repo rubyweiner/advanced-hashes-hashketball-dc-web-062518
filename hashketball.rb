6a98d347216d594c9586c2814433b2ce28dc2d2b
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 20:12:56 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
       :colors => ["Black", "White"],
       :players => [
         {
-          "Alan Anderson",
+          "Alan Anderson" => {
             :number => "0",
             :shoe => "16",
             :points => "22",
@@ -13,6 +13,7 @@ def game_hash
             :steals => "3",
             :blocks => "1",
             :slam_dunks => "1"
+          }
         },
         {
           :player_name => "Reggie Evans",
